9ae840232667abcd031b06dfd536d9e5b8a8f7b8
Rahul Panjavani <tbs.rahulpanjavani@gmail.com>
2017-11-16 18:18:18 +0530

    changes

---
+++ Mycontact/Mycontact/ContactData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mycontact
+{
+   public class ContactData
+    {
+        public async Task<List<MobileUserContact>> Fetchdata()
+        {
+          var result = await DependencyService.Get<Icall>().GetAllContact();
+            return result;
+        }
+    }
+}
